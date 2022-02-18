2a6fb9f5663fa151445d56be7cad1bf1f744c2ba
jasonfritsche <fritsche.jason@gmail.com>
2022-02-17 20:19:10 -0600

    fun fun fun

--- app.component.ts
+++ app.component.ts
@@ -1,17 +1,65 @@
-import { Component } from '@angular/core';
-import { AppService } from './services/app.service';
+import { Component } from "@angular/core";
+import { concat, concatMap, map, Observable, of, takeLast } from "rxjs";
+import { ITodo } from "./interfaces/Todo";
+import { AppService } from "./services/app.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'rxjs-sandbox';
+  title = "rxjs-sandbox";
+
+  todos$!: Observable<ITodo[]>;
+
+  todos: ITodo[] = [];
+
+  todoIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+  todoIdsTwo = [21, 22, 23, 24, 25];
+
+  ofTodoIds = of(...this.todoIds);
+  ofTodoIdsTwo = of(...this.todoIdsTwo);
 
   constructor(private appService: AppService) {}
 
   ngOnInit() {
-    this.appService.getTodos().subscribe((todos) => console.log(todos));
+    // this.todos$ = this.appService.getTodos();
+    this.getTodos();
   }
+
+  // private getTodos() {
+  //   this.ofTodoIds.pipe(concatMap((todoItem) => this.appService.getTodo(todoItem))).subscribe((result: ITodo) => this.todos.push(result));
+  // }
+
+  private getTodos() {
+    const allTodoItems = concat(this.ofTodoIds, this.ofTodoIdsTwo);
+
+    allTodoItems.subscribe((todoId) =>
+      this.appService
+        .getTodo(todoId)
+        .pipe(map((todo) => this.capitalizeTodoTitle(todo)))
+        .subscribe((todo) => this.todos.push(todo))
+    );
+  }
+
+  private capitalizeTodoTitle(todoToChange: ITodo): ITodo {
+    return {
+      ...todoToChange,
+      title: todoToChange.title
+        .split(" ")
+        .map((word) => word[0].toUpperCase() + word.substring(1))
+        .join(" "),
+    };
+  }
+
+  // private getTodos() {
+  //   const allTodoItems = concat(this.ofTodoItemsOne, this.ofTodoItemsTwo);
+
+  //   allTodoItems
+  //     .pipe(concatMap((todoItem) => this.appService.getTodo(todoItem)))
+  //     .pipe(takeLast(7))
+  //     .subscribe((result: ITodo) => this.todos.push(result));
+  // }
 }
--- services/app.service.ts
+++ services/app.service.ts
@@ -1,15 +1,20 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { ITodo } from "../interfaces/Todo";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class AppService {
   constructor(private httpClient: HttpClient) {}
 
-  private todosUrl = 'https://jsonplaceholder.typicode.com/todos';
+  private todosUrl = "https://jsonplaceholder.typicode.com/todos";
 
   getTodos() {
-    return this.httpClient.get(`${this.todosUrl}`);
+    return this.httpClient.get<ITodo[]>(`${this.todosUrl}`);
+  }
+
+  getTodo(todoItem: number) {
+    return this.httpClient.get<ITodo>(`${this.todosUrl}/${todoItem}`);
   }
 }
